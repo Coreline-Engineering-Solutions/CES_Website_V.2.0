0b704d8c76b32b19eafea40e12973a2b113437db
Tiaan Nel <tiaan@corelineengineering.com>
2024-07-08 11:12:13 +0200

    CORS Allowed on login.php

--- src/php/login.php
+++ src/php/login.php
@@ -1,5 +1,26 @@
 <?php
 
+    // CORS SOLVER
+        // Allow from any origin
+    header("Access-Control-Allow-Origin: *");
+
+    // Allow specific HTTP methods
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+    // Allow specific headers
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+    // Handle preflight requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        http_response_code(200);
+        exit;
+    }
+
+
+
+
+
+
     include "connections.php";
 
 
